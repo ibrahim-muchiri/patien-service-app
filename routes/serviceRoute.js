86e5e9506b0def6577b05779dc1663e7cf772748
ibrahim-muchiri <ibrahimainge@gmail.com>
2022-05-29 22:15:03 +0300

    modified

--- routes/serviceRoute.js
+++ routes/serviceRoute.js
@@ -5,10 +5,10 @@ const authController = require('./../controllers/authController');
 const router = express.Router();
 
 router.route('/').get(authController.protect, serviceController.getAllService)
-.post(authController.protect, authController.restrictTo('admin'), serviceController.createService);
+.post(authController.protect, authController.restrictTo('Admin'), serviceController.createService);
 
 router.route('/:id').get(authController.protect, serviceController.getService)
-.patch(authController.protect, serviceController.updateService)
-.delete(authController.protect, serviceController.deleteService);
+.patch(authController.protect, authController.restrictTo('Admin'), serviceController.updateService)
+.delete(authController.protect, authController.restrictTo('Admin'), serviceController.deleteService);
 
 module.exports = router;
